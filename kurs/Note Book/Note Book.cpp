6362c78572e9cc164f9adca1aefec705cfb360a1
Werrel <berserker2288@gmail.com>
2019-06-04 23:51:05 +0500

    1

---
+++ kurs/Note Book/Note Book.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+template<class K, class V>
+class NoteBook
+{
+	class List
+	{
+	public:
+		K key;
+		V value;
+		Lines() {}
+		Lines(K key, V value)
+		{
+			this->key = key;
+			this->value = value;
+		}
+	};
+	int listSize = 0;
+	Lines** array;
+	void IncreaseArraySize()
+	{
+		Lines** newArray = new Lines * [listSize + 1];
+		for (int i = 0; i < listSize; i++)
+			newArray[i] = array[i];
+		array = newArray;
+	};
+	int main()
+	{
+
+	}
