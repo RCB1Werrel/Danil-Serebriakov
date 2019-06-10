4f7719c132048a54954eae96e959735ec9d4b0dc
Werrel <berserker2288@gmail.com>
2019-06-11 02:08:40 +0500

    1

--- kurs/Note Book/Note Book.cpp
+++ kurs/Note Book/Note Book.cpp
@@ -1,33 +1,80 @@
 ﻿#include <iostream>
 #include <string.h>
+#include <fstream>
 
 using namespace std;
 
 
 template<class K, class V>
-class NoteBook
+class t_collection
 {
-	class List
+	class element
 	{
 	public:
 		K key;
 		V value;
-		Lines() {}
-		Lines(K key, V value)
+		element() {}
+		element(K key, V value)
 		{
 			this->key = key;
 			this->value = value;
 		}
 	};
-	int listSize = 0;
-	Lines** array;
+
+	int sizeCollection = 0;
+	element** array;
 	void IncreaseArraySize()
 	{
-		Lines** newArray = new Lines * [listSize + 1];
-		for (int i = 0; i < listSize; i++)
+		element** newArray = new element * [sizeCollection + 1];
+		for (int i = 0; i < sizeCollection) {
 			newArray[i] = array[i];
+			i = i + 1;
+		}
 		array = newArray;
-	};
+	}
+	void DecreaseArraySize()
+	{
+		element** newArray = new element * [sizeCollection - 1];
+		for (int i = 0; i < sizeCollection - 1) {
+			newArray[i] = array[i];
+			i = i + 1;
+		}
+		array = newArray;
+	}
+public:
+	t_collection()
+	{
+		sizeCollection = 0;
+		array = new element * [sizeCollection];
+		for (int i = 0; i < sizeCollection; i++)
+		{
+			array[i] = new CollectionElem();
+		}
+	}
+	int get_size()
+	{
+		return sizeCollection;
+	}
+	void add_value(V value, K key)
+	{
+		for (int i = 0; i < sizeCollection)
+		{
+			
+			if (key == array[i]->key)
+				array[i]->value = value;
+			i = i + 1;
+		}
+	}
+	friend istream& operator >> (istream& inp, t_collection& inst)
+	{
+			K key;
+			V* value = new V;
+			inp >> key;
+			inp >> *value;
+			inst.Add(key, *value);
+		return inp;
+	}
+
 	int main()
 	{
 
